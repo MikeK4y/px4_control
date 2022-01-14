9252d355418f6687927a871c78392cfb85a02db3
Mike Kalaitzakis <mike.kalaitzakis@gmail.com>
2022-01-14 17:58:46 -0500

    Finished with acados ocp formulation. Needs testing

---
+++ nmpc/scripts/acados_settings.py
@@ -0,0 +1,126 @@
+from acados_template import AcadosOcp, AcadosOcpSolver, acados_ocp
+from drone_model import drone_model
+import numpy as np
+import scipy.linalg
+
+
+def acados_settings(Tf, N):
+    """
+      Set's up the acados Optimal Control Problem and Solver
+
+      ...
+
+      Parameters
+      ----------
+      Tf : double
+        Prediction horizon
+      N : int
+        Prediction steps
+    """
+
+    # Get drone model
+    model = drone_model()
+
+    # acados OCP handle
+    ocp = AcadosOcp()
+
+    # OCP dimensions
+    nx = model.x.size()[0]
+    nu = model.u.size()[0]
+    ny = nx + nu
+    ocp.dims.N = N
+
+    # OCP costs
+    ocp.cost.cost_type = "LINEAR_LS"
+    ocp.cost.cost_type_e = "LINEAR_LS"
+
+    # State map
+    Vx = np.zeros((ny, nx))
+    Vx[:nx, :nx] = np.eye(nx)
+    ocp.cost.Vx = Vx
+
+    # Input map
+    Vu = np.zeros((ny, nu))
+    Vu[nx:, :nu] = np.eye(nu)
+    ocp.cost.Vu = Vu
+
+    # Terminal cost map
+    Vx_e = np.eye(nx)
+    ocp.cost.Vx_e = Vx_e
+
+    # State and input cost
+    W = np.diag([1e2, 1e2, 1e2,     # Position
+                 1, 1, 1,           # Velocity
+                 1e-1, 1e-1, 1e-1,  # Attitude
+                 1e-1,              # Yaw rate
+                 1e-1,              # Pitch
+                 1e-1,              # Roll
+                 1e-1])             # Thrust
+
+    # Stage cost
+    ocp.cost.W = W
+
+    # Terminal cost
+    ocp.cost.W_e = N * np.diag([1e2, 1e2, 1e2,      # Position
+                                1, 1, 1,            # Velocity
+                                1e-1, 1e-1, 1e-1])  # Attitude
+
+    # References
+    ocp.cost.yref = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    ocp.cost.yref_e = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    # OCP constraints
+    # Attitude constraints
+    pitch_min = -0.2*np.pi
+    pitch_max = 0.2*np.pi
+    roll_min = -0.2*np.pi
+    roll_max = 0.2*np.pi
+    yaw_min = -np.pi
+    yaw_max = np.pi
+
+    ocp.constraints.idxbx = np.array([6, 7, 8])
+    ocp.constraints.lbx = np.array([roll_min, pitch_min, yaw_min])
+    ocp.constraints.ubx = np.array([roll_max, pitch_max, yaw_max])
+
+    # Input constraints
+    yaw_rate_min = -0.5*np.pi
+    yaw_rate_max = 0.5*np.pi
+    pitch_cmd_min = -0.2*np.pi
+    pitch_cmd_max = 0.2*np.pi
+    roll_cmd_min = -0.2*np.pi
+    roll_cmd_max = 0.2*np.pi
+    thrust_min = -1.0
+    thrust_max = 1.0
+
+    ocp.constraints.idxbu = np.array([0, 1, 2, 3])
+    ocp.constraints.lbu = np.array(
+        [yaw_rate_min, pitch_cmd_min, roll_cmd_min, thrust_min])
+    ocp.constraints.ubu = np.array(
+        [yaw_rate_max, pitch_cmd_max, roll_cmd_max, thrust_max])
+
+    # Initial state
+    ocp.constraints.x0 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    # Model
+    ocp.model = model
+    ocp.parameter_values = np.array(
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    # OCP options
+    ocp.solver_options.tf = Tf
+    ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM'
+    ocp.solver_options.nlp_solver_type = 'SQP_RTI'
+    ocp.solver_options.hessian_approx = 'GAUSS_NEWTON'
+    ocp.solver_options.integrator_type = 'ERK'
+
+    # Path to acados include and lib directories
+    # ocp.acados_include_path = '../../acados/include'
+    # ocp.acados_lib_path = '../../acados/lib'
+
+    acados_solver = AcadosOcpSolver(ocp, json_file="acados_ocp.json")
+
+    return acados_solver
+
+
+acados_settings(1, 20)
+print("Acados NMPC generated")
---
+++ nmpc/scripts/drone_model.py
@@ -0,0 +1,89 @@
+from acados_template import AcadosModel
+from casadi import SX, vertcat, sin, cos
+
+
+def drone_model():
+
+    model_name = 'drone_w_disturbances'
+
+    # State
+    px = SX.sym('px')    # Position x
+    py = SX.sym('py')    # Position y
+    pz = SX.sym('pz')    # Position z
+    vx = SX.sym('vx')    # Velocity x
+    vy = SX.sym('vy')    # Velocity y
+    vz = SX.sym('vz')    # Velocity z
+    qx = SX.sym('qx')    # Roll
+    qy = SX.sym('qy')    # Pitch
+    qz = SX.sym('qz')    # Yaw
+    x = vertcat(px, py, pz,
+                vx, vy, vz,
+                qx, qy, qz)
+
+    # Input
+    u1 = SX.sym('u1')  # Yaw rate
+    u2 = SX.sym('u2')  # Pitch
+    u3 = SX.sym('u3')  # Roll
+    u4 = SX.sym('u4')  # Thrust
+    u = vertcat(u1, u2, u3, u4)
+
+    # Model parameters
+    tp = SX.sym('tp')    # Roll
+    kp = SX.sym('kp')    # Roll
+    tr = SX.sym('tr')    # Pitch
+    kr = SX.sym('kr')    # Pitch
+    dx = SX.sym('dx')    # Damping x
+    dy = SX.sym('dy')    # Damping y
+    dz = SX.sym('dz')    # Damping z
+    fdx = SX.sym('fdx')  # Disturbance force x
+    fdy = SX.sym('fdy')  # Disturbance force y
+    fdz = SX.sym('fdz')  # Disturbance force z
+    kth = SX.sym('kth')  # Thrust coefficients
+    g = SX.sym('g')  # Gravity
+    p = vertcat(tp, kp,
+                tr, kr,
+                dx, dy, dz,
+                fdx, fdy, fdz,
+                kth, g)
+
+    # F_impl
+    px_dot = SX.sym('px_dot')
+    py_dot = SX.sym('py_dot')
+    pz_dot = SX.sym('pz_dot')
+    vx_dot = SX.sym('vx_dot')
+    vy_dot = SX.sym('vy_dot')
+    vz_dot = SX.sym('vz_dot')
+    qx_dot = SX.sym('qx_dot')
+    qy_dot = SX.sym('qy_dot')
+    qz_dot = SX.sym('qz_dot')
+    x_dot = vertcat(px_dot, py_dot, pz_dot,
+                    vx_dot, vy_dot, vz_dot,
+                    qx_dot, qy_dot, qz_dot)
+
+    # Dynamics
+    dpx = vx
+    dpy = vy
+    dpz = vz
+    dvx = dx * vx + (cos(qz)*sin(qy)*cos(qx) - sin(qz)*sin(qx)) * kth * u4 + fdx
+    dvy = dy * vy + (sin(qz)*sin(qy)*cos(qx) - cos(qz)*sin(qx)) * kth * u4 + fdy
+    dvz = dz * vz + (cos(qy)*cos(qx)) * kth * u4 + g + fdz
+    dqx = (kr * u3 - qx) / tr
+    dqy = (kp * u2 - qy) / tp
+    dqz = u1
+
+    f_expl = vertcat(dpx, dpy, dpz,
+                     dvx, dvy, dvz,
+                     dqx, dqy, dqz)
+    f_impl = x_dot - f_expl
+
+    # Acados model
+    model = AcadosModel()
+    model.f_impl_expr = f_impl
+    model.f_expl_expr = f_expl
+    model.x = x
+    model.xdot = x_dot
+    model.u = u
+    model.p = p
+    model.name = model_name
+
+    return model
